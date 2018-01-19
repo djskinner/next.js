b30946ac4f63534393133c615a636f1ed645afd9
Dan Skinner <dan.skinner@skybettingandgaming.com>
2018-01-19 15:57:34 +0000

    externalise styles (does not work)

--- examples/with-typescript/pages/index.tsx
+++ examples/with-typescript/pages/index.tsx
@@ -1,9 +1,12 @@
 import Link from 'next/link'
 
+import styles from '../style'
+
 export default () =>
   <div>
     Hello World.{' '}
     <Link href="/about">
       <a>About</a>
     </Link>
+		<style jsx>{styles}</style>
   </div>
---
+++ examples/with-typescript/style.ts
@@ -0,0 +1,8 @@
+// tslint:disable-next-line:no-submodule-imports
+import css from "styled-jsx/css";
+
+export default css`
+.inGame {
+    background: crimson;
+}
+`
